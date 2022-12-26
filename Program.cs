ed4bde8cf5ecca2e963caf81ae3a55f2c78d9e9c
vijay <vijaydurkikar1@gmail.com>
2022-12-26 12:18:03 +0530

    Greeting Message

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿namespace HelloWorld
+{
+    class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            int num1 = 14;
+
+            double num2 = 3.3;
+
+            char char2 = 'a';
+
+            bool value = true;
+
+            string str = "Bridgelabz";
+
+            Console.WriteLine("The integer value is : " +num1);
+
+            Console.WriteLine("The double value is : " + num2);
+
+            Console.WriteLine("The charcter value is : " + char2);
+
+            Console.WriteLine("The boolean value is : " + value);
+
+            Console.WriteLine("The string value is : " + str);
+        }
+
+    }
+}
